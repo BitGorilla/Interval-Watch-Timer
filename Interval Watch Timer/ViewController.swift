0ee3efb27d410cc481d9d01a6ac3297041f52100
Martin <martinsjolund@hotmail.com>
2018-10-01 13:30:01 +0200

    Initial Commit

---
+++ Interval Watch Timer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Interval Watch Timer
+//
+//  Created by Martin Sjölund on 2018-10-01.
+//  Copyright © 2018 Martin Sjölund. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Interval Watch TimerTests/Interval_Watch_TimerTests.swift
@@ -0,0 +1,36 @@
+//
+//  Interval_Watch_TimerTests.swift
+//  Interval Watch TimerTests
+//
+//  Created by Martin Sjölund on 2018-10-01.
+//  Copyright © 2018 Martin Sjölund. All rights reserved.
+//
+
+import XCTest
+@testable import Interval_Watch_Timer
+
+class Interval_Watch_TimerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
